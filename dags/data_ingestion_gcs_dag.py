d3fae4dd4c0f22608a36f7ea3cef0650f107cc5d
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2022-03-08 18:41:25 -0600

    Got unzip working

--- dags/data_ingestion_gcs_dag.py
+++ dags/data_ingestion_gcs_dag.py
@@ -7,6 +7,7 @@ from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+
 from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 
@@ -19,12 +20,13 @@ PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
 #https://divvy-tripdata.s3.amazonaws.com/202202-divvy-tripdata.zip
-year = '{:04d}'.format(datetime.now().year)
-month ='{:02d}'.format(datetime.now().month-1)
-dataset_file = year+month +'-divvy-tripdata.zip'
-dataset_url = f"https://divvy-tripdata.s3.amazonaws.com/{dataset_file}"
+# year = '{:04d}'.format(datetime.now().year)
+# month ='{:02d}'.format(datetime.now().month-1)
+dataset_zip = "{{ execution_date.strftime(\'%Y%m\') }}-divvy-tripdata.zip"
+csv_file = "{{ execution_date.strftime(\'%Y%m\') }}-divvy-tripdata.csv"
+dataset_url = f"https://divvy-tripdata.s3.amazonaws.com/{dataset_zip}"
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
+parquet_file = csv_file.replace('.csv', '.parquet')
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'divvy_trips')
 
 
@@ -66,8 +68,7 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 default_args = {
     "owner": "airflow",
-    "start_date": days_ago(1),
-    "depends_on_past": False,
+    "depends_on_past": True,
     "retries": 1,
 }
 
@@ -84,27 +85,32 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_zip}"
+    )
+
+    unzip_data_file = BashOperator(
+        task_id = "unzip_data_file",
+        bash_command = f"cd /opt/airflow && unzip {path_to_local_home}/{dataset_zip} && rm *.zip && rm __MACOS*"
     )
 
     format_to_parquet_task = PythonOperator(
         task_id="format_to_parquet_task",
         python_callable=format_to_parquet,
         op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
+            "src_file": f"{path_to_local_home}/{csv_file}",
         },
     )
 
     # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
-        },
-    )
+    # local_to_gcs_task = PythonOperator(
+    #     task_id="local_to_gcs_task",
+    #     python_callable=upload_to_gcs,
+    #     op_kwargs={
+    #         "bucket": BUCKET,
+    #         "object_name": f"raw/{parquet_file}",
+    #         "local_file": f"{path_to_local_home}/{parquet_file}",
+    #     },
+    # )
 
     # bigquery_external_table_task = BigQueryCreateExternalTableOperator(
     #     task_id="bigquery_external_table_task",
@@ -121,5 +127,5 @@ with DAG(
     #     },
     # )
 
-    download_dataset_task >> format_to_parquet_task 
+    download_dataset_task >> unzip_data_file >> format_to_parquet_task 
     # >> local_to_gcs_task >> bigquery_external_table_task
